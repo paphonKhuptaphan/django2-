aa63a03cab79cf86a4eb4463308844aab6388396
paphonKhuptaphan <paphonKhuptaphan>
2024-08-28 16:30:13 +0700

    first commit

---
+++ myweb/blogs/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def blogs(request):
+  template = loader.get_template('blogs.html')
+  return HttpResponse(template.render())
